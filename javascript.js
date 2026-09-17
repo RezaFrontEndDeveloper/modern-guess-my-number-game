12355f5a4cda83aa463065cd8a628bad06ed3017
RezaFrontEndDeveloper <rezablackcamel@gmail.com>
2026-09-17 15:26:55 +0330

    fixed giude boxes

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ const downBox = document.querySelector(".guide_box_down");
 const status1 = document.querySelector(".status");
 const guessNumber = document.querySelector(".guess_number");
 const guessRecord = document.querySelector(".guess_record");
-const resetBtn = document.querySelector(".btn");
+const resetBtn = document.querySelector(".reset");
 let score = 20;
 let record = 0;
 guessRecord.textContent = record;
@@ -27,18 +27,24 @@ uiButton.addEventListener("click", function () {
     status1.textContent = "برو بالاتر";
     score--;
     guessNumber.textContent = score;
+    upBox.classList.add("active_box");
+    downBox.classList.remove("active_box");
   }
 
   if (number < Number(clientValue.value)) {
     status1.textContent = "بیا پایین سرمون درد گرفت";
     score--;
     guessNumber.textContent = score;
+    upBox.classList.remove("active_box");
+    downBox.classList.add("active_box");
   }
 
   if (number === Number(clientValue.value)) {
     status1.textContent = "درسته ";
     guessNumber.textContent = score;
-
+    upBox.classList.remove("active_box");
+    downBox.classList.remove("active_box");
+    okBox.classList.add("active_box_ok");
     if (score > record) {
       record = score;
       guessRecord.textContent = record;
