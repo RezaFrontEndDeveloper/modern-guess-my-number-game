b861c5fdcd9d03052362f2c9e7c132f1c096503a
RezaFrontEndDeveloper <rezablackcamel@gmail.com>
2026-09-17 15:15:52 +0330

    intial commit

---
+++ javascript.js
@@ -0,0 +1,54 @@
+"use strict";
+function randomNumber() {
+  return Math.floor(Math.random() * 11);
+}
+let number = randomNumber();
+console.log(number);
+const clientValue = document.querySelector(".input");
+const uiButton = document.querySelector(".form_button");
+const upBox = document.querySelector(".guide_box_up ");
+const okBox = document.querySelector(".guide_box_ok");
+const downBox = document.querySelector(".guide_box_down");
+const status1 = document.querySelector(".status");
+const guessNumber = document.querySelector(".guess_number");
+const guessRecord = document.querySelector(".guess_record");
+const resetBtn = document.querySelector(".btn");
+let score = 20;
+let record = 0;
+guessRecord.textContent = record;
+console.log(score);
+
+uiButton.addEventListener("click", function () {
+  if (!clientValue) {
+    status1.textContent = "یک مقدار وارد کنید ";
+  }
+
+  if (number > Number(clientValue.value)) {
+    status1.textContent = "برو بالاتر";
+    score--;
+    guessNumber.textContent = score;
+  }
+
+  if (number < Number(clientValue.value)) {
+    status1.textContent = "بیا پایین سرمون درد گرفت";
+    score--;
+    guessNumber.textContent = score;
+  }
+
+  if (number === Number(clientValue.value)) {
+    status1.textContent = "درسته ";
+    guessNumber.textContent = score;
+
+    if (score > record) {
+      record = score;
+      guessRecord.textContent = record;
+    }
+  }
+});
+
+resetBtn.addEventListener("click", function () {
+  score = 20;
+  number = randomNumber();
+  console.log("again", number);
+  guessNumber.textContent = score;
+});
